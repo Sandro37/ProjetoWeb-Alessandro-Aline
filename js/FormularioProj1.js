e9f81e1db51330ed9ea6bb44642eaf90c898f19e
Sandro37 <alessandro37@outlook.com.br>
2020-12-19 19:36:40 -0300

    Adicionando páginas e funcções novas
    Foi adicionado novas páginas e um mapa com dois pontos de campinas

---
+++ js/FormularioProj1.js
@@ -0,0 +1,12 @@
+function pegarDados(){
+    if(document.getElementById("nome").value == ""){
+    }else if(document.getElementById("email").value == ""){
+    }else if(document.getElementById("comentarios").value == ""){
+    }else{
+        let nome = document.getElementById("nome").value;
+        let email = document.getElementById("email").value;
+        let comentarios = document.getElementById("comentarios").value;
+
+        alert("Dados Enviados.");
+    }
+}
